886465e8ad7673845504fcb4302c076c5c2b9b8a
zedzior <piotr.zabieglik@gmail.com>
2020-03-31 00:58:41 +0200

    add basic logic, handle click events, wait 1 second after pair of puzzles is being shown, add comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,103 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import utils from './utils';
 
 const Puzzle = (props) => {
-  const [color, setColor] = useState('grey');
-  const changeColor = () => setColor('white');
-
   return (
     <div className='puzzle'
-      style={{ backgroundColor: color }}
-      onClick={changeColor}
+      onClick={() => props.onClick(props.index, props.number)}
+      style={{ backgroundColor: colors[props.status(props.index, props.number)] }}
     >
-      {props.id}
+      {props.number}
     </div>
   );
 };
 
-const Game = () => {
-  const range = utils.getRandomPairs(1, 8);
+const Game = (props) => {
+  const range = props.range;
+  const [pairId, setPairId] = useState([]);
+  const [pairVal, setPairVal] = useState([]);
+  const [matched, setMatched] = useState([]);
+  const [lock, setLock] = useState(false);
+
+  useEffect(() => {
+    //if locked, wiat one second and unlock and clear pair arrays
+    if (lock == true) {
+      setTimeout(() => {
+        console.log('...');
+        setLock(false);
+        setPairVal([]);
+        setPairId([]);
+      }, 1000);
+    }
+  });
+
+  const puzzStatus = (id, val) => {
+    if (pairId.includes(id)) {
+      return 'open';
+    }
+    if (matched.includes(val)) {
+      return 'matched';
+    }
+    return 'hidden';
+  };
+
+  const puzzClick = (id, val) => {
+    //if lock == true or the same button was clicked do nothing when click
+    if (lock == true || id == pairId[0]) {
+      return;
+    }
+    //if there is 0 or 1 opened, but not matched
+    const newPairId = pairId.concat(id);
+    const newPairVal = pairVal.concat(val);
+    if (pairId.length <= 1 && !matched.includes(val)) {
+      //lock game for a while, when 2 puzzles are opened
+      if (pairId.length == 1) {
+        setLock(true);
+      }
+      //add clicked buttons to pair arrays
+      setPairId(newPairId);
+      setPairVal(newPairVal);
+    }
+    // if there is correct match
+    if (newPairVal[0] === newPairVal[1] && newPairVal.length == 2) {
+      const newMatched = matched.concat(pairVal[0]);
+      setMatched(newMatched);
+      //clear pair arrays
+      setPairId([]);
+      setPairVal([]);
+      //unlock game
+      setLock(false);
+    }
+  };
+
   return (
     <>
       <header>
         Mastermind by Zedziorowsky for three!
       </header>
       <div className='board'>
-        {range.map((number) => <Puzzle key={number} id={number} />)}
+        {range.map((number, index) =>
+          <Puzzle
+            key={index}
+            index={index}
+            number={number}
+            status={puzzStatus}
+            onClick={puzzClick}
+          />)}
       </div>
     </>
   );
 };
 
+const colors = {
+  matched: 'green',
+  open: 'white',
+  hidden: 'grey',
+};
 
 export function App() {
+  const range = utils.getRandomPairs(1, 8);
   return (
-    <Game />
+    <Game range={range} />
   );
 }
